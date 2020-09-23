52b7121f843e426ace14d8be8238d55946a4e053
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-09-23 02:18:49 -0500

    changes scss

--- src/view/public/Register/components/RegisterForm.jsx
+++ src/view/public/Register/components/RegisterForm.jsx
@@ -33,26 +33,28 @@ const RegisterForm = () => {
   return (
     <Formik validationSchema={LoginValidation} initialValues={initialValues} onSubmit={onCompleteForm}>
       <Col xs="12" sm="12" md="6" lg="4" xl="3" className="mx-auto">
-        <Form>
-          <div className="d-flex flex-column align-items-center">
-            <Link to="/">
-              <img
-                className="login_logo mt-3 mb-3 mr-auto ml-auto"
-                src={Logo} alt="logo" />
-            </Link>
-          </div>
-          <div className="card card-body h-100 justify-content-div">
-            <p className="title-register pb-2">Create Account</p>
-            <FormField name="displayName" label="Your Name" />
-            <FormField name="email" label="E-mail" />
-            <FormField name="password" component="password" label="Password" />
-            <FormField name="repassword" component="password" label="Re-enter password" />
-            <small>By creating an account, you agree to Cubizon's Conditions of Use and Privacy Notice.</small>
-            <button type="submit" className="mt-5 registerbutton btn btn-md btn-primary btn-block mt-3">
-              Create your Cubizon account
+        <div className="register">
+          <Form>
+            <div className="d-flex flex-column align-items-center">
+              <Link to="/">
+                <img
+                  className="register-logo mt-3 mb-3 mr-auto ml-auto"
+                  src={Logo} alt="logo" />
+              </Link>
+            </div>
+            <div className="card card-body h-100 justify-content-div">
+              <p className="title-register pb-2">Create Account</p>
+              <FormField name="displayName" label="Your Name" />
+              <FormField name="email" label="E-mail" />
+              <FormField name="password" component="password" label="Password" />
+              <FormField name="repassword" component="password" label="Re-enter password" />
+              <small>By creating an account, you agree to Cubizon's Conditions of Use and Privacy Notice.</small>
+              <button type="submit" className="mt-5 registerbutton btn btn-md btn-primary btn-block mt-3">
+                Create your Cubizon account
             </button>
-          </div>
-        </Form>
+            </div>
+          </Form>
+        </div>
       </Col>
     </Formik>
   )
